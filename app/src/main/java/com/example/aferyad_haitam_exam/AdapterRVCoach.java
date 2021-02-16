6410876b6407d7c23fd329b3869ecfe01ce942e1
Haferyad <haitamaferyad@gmail.com>
2021-02-16 14:20:28 +0100

    recyclerview

--- app/src/main/java/com/example/aferyad_haitam_exam/AdapterRVCoach.java
+++ app/src/main/java/com/example/aferyad_haitam_exam/AdapterRVCoach.java
@@ -1,31 +1,70 @@
 package com.example.aferyad_haitam_exam;
 
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.example.aferyad_haitam_exam.Model.Coach;
+
+import java.util.ArrayList;
+
 public class AdapterRVCoach extends RecyclerView.Adapter<AdapterRVCoach.ViewHolder> {
+    Context context;
+    ArrayList<Coach> coaches;
+
+    public AdapterRVCoach(Context context, ArrayList<Coach> coaches) {
+        this.context = context;
+        this.coaches = coaches;
+    }
+
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        Context context=parent.getContext();
+        LayoutInflater inflater=LayoutInflater.from(context);
+        View coachView= inflater.inflate(R.layout.item_card,parent,false);
+        return new AdapterRVCoach.ViewHolder(coachView);
     }
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        Coach coach=coaches.get(position);
 
+        ImageView imageView=holder.imageView;
+        TextView name=holder.name;
+        TextView post=holder.post;
+
+
+        Glide.with(context).load(coach.getPhoto())
+                .diskCacheStrategy(DiskCacheStrategy.ALL)
+                .into(imageView);
+        name.setText(coach.getName());
+
+        post.setText(coach.getPosition());
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return coaches.size();
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder {
+        ImageView imageView;
+        TextView name;
+        TextView post;
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
+            imageView=itemView.findViewById(R.id.imagepers);
+            name=itemView.findViewById(R.id.namepers);
+            post=itemView.findViewById(R.id.postpers);
         }
     }
 }
--- app/src/main/java/com/example/aferyad_haitam_exam/AdapterRVPlayer.java
+++ app/src/main/java/com/example/aferyad_haitam_exam/AdapterRVPlayer.java
@@ -1,31 +1,72 @@
 package com.example.aferyad_haitam_exam;
 
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.example.aferyad_haitam_exam.Model.Player;
+
+import java.util.ArrayList;
+
 public class AdapterRVPlayer extends RecyclerView.Adapter<AdapterRVPlayer.ViewHolder> {
+    Context context;
+    ArrayList<Player> players;
+
+    public AdapterRVPlayer(Context context, ArrayList<Player> players) {
+        this.context = context;
+        this.players = players;
+    }
+
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        Context context=parent.getContext();
+        LayoutInflater inflater=LayoutInflater.from(context);
+        View playerView= inflater.inflate(R.layout.item_player,parent,false);
+        return new ViewHolder(playerView);
     }
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        Player player=players.get(position);
 
+        ImageView imageView=holder.imageView;
+        TextView name=holder.name;
+        TextView post=holder.post;
+        TextView number=holder.number;
+
+        Glide.with(context).load(player.getPhoto())
+                .diskCacheStrategy(DiskCacheStrategy.ALL)
+                .into(imageView);
+        name.setText(player.getName());
+        number.setText(player.getNumber());
+        post.setText(player.getPosition());
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return players.size();
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder {
+        ImageView imageView;
+        TextView name;
+        TextView post;
+        TextView number;
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
+            imageView=itemView.findViewById(R.id.imagepers);
+            name=itemView.findViewById(R.id.namepers);
+            post=itemView.findViewById(R.id.postpers);
+            number=itemView.findViewById(R.id.number);
         }
     }
 }
